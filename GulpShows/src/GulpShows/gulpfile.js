50b4242fa8feb2beba4a9e8934153f839b1ddec4
Angel G. <garaydev@gmail.com>
2016-11-29 00:02:34 -0500

    Added gulpjs tasks

--- GulpShows/src/GulpShows/gulpfile.js
+++ GulpShows/src/GulpShows/gulpfile.js
@@ -3,8 +3,39 @@ This file in the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var gulp = require('gulp');
+// JavaScript source code
+"use strict";
 
-gulp.task('default', function () {
-    // place code for your default task here
+var gulp = require('gulp'),
+      concat = require('gulp-concat'),
+      uglify = require('gulp-uglify'),
+      rename = require('gulp-rename'),
+      maps = require('gulp-sourcemaps'),
+	  jshint = require('gulp-jshint');
+
+gulp.task("concatJS", function () {
+    return gulp.src([
+        'Scripts/main.js',
+        'Scripts/double.js'
+    ])
+        .pipe(maps.init())
+        .pipe(concat('app.js'))
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('Scripts'));
+});
+
+gulp.task("minifyJS", ['concatJS'], function () {
+    return gulp.src("Scripts/app.js")
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('Scripts'));
+});
+
+gulp.task("jsHint", function () {
+    return gulp.src('Scripts/main.js')
+    .pipe(jshint())
+	.pipe(jshint.reporter('jshint-stylish', { verbose: true }));
+});
+
+gulp.task("default", ["minifyJS"], function () {
 });
